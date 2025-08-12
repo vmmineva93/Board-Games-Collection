8d80d056cbcc054135096e8dc53b8d9d750d75bd
vmmineva <veronika.mineva93@gmail.com>
2025-08-13 00:18:12 +0300

    Add edit, add and current game components

---
+++ client/src/app/single-game/current-game/current-game.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-game',
+  standalone: true, 
+  imports: [],
+  templateUrl: './current-game.component.html',
+  styleUrl: './current-game.component.css'
+})
+export class CurrentGameComponent {
+
+}
---
+++ client/src/app/single-game/edit-game/edit-game.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-game',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-game.component.html',
+  styleUrl: './edit-game.component.css'
+})
+export class EditGameComponent {
+
+}
