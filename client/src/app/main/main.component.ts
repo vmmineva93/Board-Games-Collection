1d216b05f9f57393f8e62d86800ef0a1b7b84481
vmmineva <veronika.mineva93@gmail.com>
2025-08-13 16:26:22 +0300

    Add main and loader components

--- client/src/app/main/main.component.ts
+++ client/src/app/main/main.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user/user.service.js';
+import { HomeComponent } from '../home/home.component.js';
+import { CollectionComponent } from '../collection/collection.component.js';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [HomeComponent, CollectionComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
 
+  constructor(private userService: UserService) {}
 }
