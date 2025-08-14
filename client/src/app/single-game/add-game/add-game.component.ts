3c13044ae70c050ce64e5f677921c93f26f8f8aa
vmmineva <veronika.mineva93@gmail.com>
2025-08-14 13:26:34 +0300

    add add-game functionality

--- client/src/app/single-game/add-game/add-game.component.ts
+++ client/src/app/single-game/add-game/add-game.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../api.service.js';
+import { Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-game',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-game.component.html',
   styleUrl: './add-game.component.css'
 })
 export class AddGameComponent {
+  constructor(private apiService: ApiService, private router: Router) {}
 
+  addGame(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    
+    const { title, players, playingTime, age, categories, imageUrl, description } = form.value;
+   
+    this.apiService.createGame(title, players, playingTime, age, categories, imageUrl, description).subscribe(() => {
+      this.router.navigate(['/collection']);
+    });
+  }
 }
---
+++ client/src/app/types/games.ts
@@ -0,0 +1,12 @@
+export interface Game {
+    _ownerId: string;
+    title: string,
+    players: string,
+    playingTime: string,
+    age: string,
+    categories: string,
+    description: string,
+    imageUrl: string,
+    _createdOn: string,
+    _id: string;
+}
