46b2b735013e9a9812b2ee4c8804fd63580c2799
vmmineva <veronika.mineva93@gmail.com>
2025-08-16 21:51:41 +0300

    Fix profile component

--- client/src/app/user/profile/profile.component.ts
+++ client/src/app/user/profile/profile.component.ts
@@ -1,16 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProfileDetails } from '../../types/user.js';
-import { emailValidator } from '../../utils/emailValidator.js';
-import { DOMAINS } from '../../constants.js';
 import { UserService } from '../user.service.js';
-import { Router } from '@angular/router';
-import { RouterLink } from "../../../../node_modules/@angular/router/router_module.d-Bx9ArA6K";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
@@ -20,25 +15,12 @@ export class ProfileComponent implements OnInit {
     email: '',
   };
 
-  form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-    ]),
-    email: new FormControl('', [Validators.required, emailValidator(DOMAINS)]),
-  });
-
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-
-
-    const { username, email } = this.userService.user!;
-    this.profileDetails = { username, email };
-
-    this.form.setValue({
-      username,
-      email,
-    });
+    const user = this.userService.user;
+    if (user) {
+      this.profileDetails = { username: user.username, email: user.email };
+    }
   }
 }
