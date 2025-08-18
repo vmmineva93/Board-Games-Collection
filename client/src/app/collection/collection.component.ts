b8393feb37b56da6946863202b0d0c04481b4421
vmmineva <veronika.mineva93@gmail.com>
2025-08-18 23:15:35 +0300

    Add search

--- client/src/app/collection/collection.component.ts
+++ client/src/app/collection/collection.component.ts
@@ -4,29 +4,39 @@ import { ApiService } from '../api.service.js';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { LoaderComponent } from '../shared/loader/loader.component.js';
 import { SlicePipe } from '../shared/pipes/slice.pipe.js';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-collection',
   standalone: true,
-  imports: [RouterLink, LoaderComponent, SlicePipe],
+  imports: [RouterLink, LoaderComponent, SlicePipe, FormsModule],
   templateUrl: './collection.component.html',
   styleUrl: './collection.component.css'
 })
 export class CollectionComponent implements OnInit {
   games: Game[] = [];
+  filteredGames: Game[] = [];
   isLoading = true;
+  searchText: string = '';
 
-  constructor(private apiService: ApiService, private route: ActivatedRoute) {}
+  constructor(private apiService: ApiService, private route: ActivatedRoute) { }
 
 
 
   ngOnInit() {
-    
+
     this.apiService.getGames().subscribe((games) => {
       this.games = games;
-      
-      this.isLoading = false; 
+      this.filteredGames = games;
+      this.isLoading = false;
     });
 
+  }
+
+  onSearch() {
+    const text = this.searchText.toLowerCase();
+    this.filteredGames = this.games.filter(game =>
+      game.title.toLowerCase().includes(text)
+    );
   }
 }
