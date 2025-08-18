6d2044cac402b221b8d5ef91a044a0c054b4059a
vmmineva <veronika.mineva93@gmail.com>
2025-08-18 23:54:54 +0300

    add reactive form for login

--- client/src/app/user/login/login.component.ts
+++ client/src/app/user/login/login.component.ts
@@ -1,33 +1,61 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../user.service.js';
 import { DOMAINS } from '../../constants.js';
 import { EmailValidationDirective } from '../../directives/email-validation.directive.js';
 import { SafeStorageService } from '../../safe-storage.service.js';
+import { emailValidator } from '../../utils/emailValidator.js';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule, FormsModule, EmailValidationDirective],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   errorMsg: string | null = null;
-  domains = DOMAINS;
+
+  form = new FormGroup ({
+    email: new FormControl('', [Validators.required, emailValidator(DOMAINS)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(5)])
+  });
 
   constructor(private userService: UserService, private router: Router, private safeStorage: SafeStorageService) {}
 
-  login(form: NgForm) {
+  isFieldTextMissing(controlName: string) {
+    return (
+      this.form.get(controlName)?.touched &&
+      this.form.get(controlName)?.errors?.['required']
+    );
+  }
+
+  get isEmailNotValid() {
+    return (
+      this.form.get('email')?.touched &&
+      this.form.get('email')?.errors?.['emailValidator']
+    );
+  }
+
+  get isNotMinPassword() {
+    return (
+      this.form.get('password')?.touched &&
+      this.form.get('password')?.errors?.['minlength']
+    );
+  }
+
+
+
+  login() {
     
-    if (form.invalid) {
+    if (this.form.invalid) {
       return;
     }
 
-    const { email, password } = form.value;
+    const { email, password } = this.form.value;
     
-    this.userService.login(email, password).subscribe((data) => {
+    this.userService.login(email!, password!).subscribe((data) => {
       const token = data.accessToken;
       this.safeStorage.setItem('X-Authorization', token);
       this.router.navigate(['/home']);
