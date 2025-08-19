9bf5926e9b5f994dac43608af375737bfbc8dfe5
vmmineva <veronika.mineva93@gmail.com>
2025-08-19 18:34:28 +0300

    Fix scrolling

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -9,7 +9,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withInterceptors([appInterceptor])),
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
-    // provideClientHydration(withEventReplay())
+    provideRouter(routes, withInMemoryScrolling({
+      scrollPositionRestoration: 'top', 
+      anchorScrolling: 'enabled'           
+    })), 
+
   ]
 };
